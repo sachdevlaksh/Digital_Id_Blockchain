5c4d737e1f230c94eda77831a65cc37171336ca8
prem09jun <prem09jun@gmail.com>
2019-04-23 11:23:03 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ var app = express();
 var multer = require('multer');
 var nodemailer = require('nodemailer');
 var Cloudant = require('@cloudant/cloudant');
+var Fabric_Client = require('fabric-client');
+var path = require('path');
+var util = require('util');
+var os = require('os');
+var { FileSystemWallet, Gateway } = require('fabric-network');
+var ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
+var ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+var ccp = JSON.parse(ccpJSON);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -335,6 +344,176 @@ var triggerEmail = async (receiverEmailId, subject, mailBody) => {
 		console.log('Issues triggering mail to applicant ! ' + err);
 		return({ success: false, message: 'Issues triggering mail to applicant ! ' });
 	}
+}
+
+//Invoke Hyperledger Fabric Chaincode
+var invoke = async (chaincodeId, channelName, userDoingTxn, functionName, arguments) => {
+	console.log('\n\n --- Invoke.js - start');
+	try {
+		console.log('Setting up client side network objects');
+		const fabric_client = new Fabric_Client();
+		const channel = fabric_client.newChannel(channelName);
+		console.log('Created client side object to represent the channel');
+		const peer = fabric_client.newPeer('grpc://localhost:7051');
+		console.log('Created client side object to represent the peer');
+		const orderer = fabric_client.newOrderer('grpc://localhost:7050')
+		console.log('Created client side object to represent the orderer');
+
+		const member_user = null;
+		const store_path = path.join(__dirname, 'wallet');
+		console.log('Setting up the user store at path:'+store_path);
+		const state_store = await Fabric_Client.newDefaultKeyValueStore({ path: store_path});
+		fabric_client.setStateStore(state_store);
+		const crypto_suite = Fabric_Client.newCryptoSuite();
+
+		const crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+		crypto_suite.setCryptoKeyStore(crypto_store);
+		fabric_client.setCryptoSuite(crypto_suite);
+
+		const user = await fabric_client.getUserContext(userDoingTxn, true);
+		if (user && user.isEnrolled()) {
+			console.log('Successfully loaded' +userDoingTxn+ 'from user store');
+		} else {
+			throw new Error('\n\nFailed to get' +userDoingTxn+ '.... run registerUser.js');
+		}
+
+		console.log('Successfully setup client side');
+		console.log('\n\nStart invoke processing');
+
+		const tx_id = fabric_client.newTransactionID();
+		console.log(util.format("\nCreated a transaction ID: %s", tx_id.getTransactionID()));
+
+		const proposal_request = {
+			targets: [peer],
+			chaincodeId: chaincodeId,
+			fcn: functionName,
+			args: arguments,
+			chainId: channelName,
+			txId: tx_id
+		};
+
+		const endorsement_results = await channel.sendTransactionProposal(proposal_request);
+
+		const proposalResponses = endorsement_results[0];
+		const proposal = endorsement_results[1];
+
+		if (proposalResponses[0] instanceof Error) {
+			console.error('Failed to send Proposal. Received an error :: ' + proposalResponses[0].toString());
+			throw proposalResponses[0];
+		} else if (proposalResponses[0].response && proposalResponses[0].response.status === 200) {
+			console.log(util.format(
+				'Successfully sent Proposal and received response: Status - %s',
+				proposalResponses[0].response.status));
+		} else {
+			const error_message = util.format('Invoke chaincode proposal:: %j', proposalResponses[i]);
+			console.error(error_message);
+			throw new Error(error_message);
+		}
+
+		const commit_request = {
+			orderer: orderer,
+			proposalResponses: proposalResponses,
+			proposal: proposal
+		};
+
+		const transaction_id_string = tx_id.getTransactionID();
+
+		const promises = [];
+
+		const sendPromise = channel.sendTransaction(commit_request);
+		promises.push(sendPromise);
+
+		let event_hub = channel.newChannelEventHub(peer);
+
+		let txPromise = new Promise((resolve, reject) => {
+
+			let handle = setTimeout(() => {
+				event_hub.unregisterTxEvent(transaction_id_string);
+				event_hub.disconnect();
+				resolve({event_status : 'TIMEOUT'});
+			}, 30000);
+
+			event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
+
+				clearTimeout(handle);
+				const return_status = {event_status : code, tx_id : transaction_id_string};
+				if (code !== 'VALID') {
+					console.error('The transaction was invalid, code = ' + code);
+					resolve(return_status);
+				} else {
+					console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
+					resolve(return_status);
+				}
+			}, (err) => {
+				reject(new Error('There was a problem with the eventhub ::'+err));
+			},
+				{disconnect: true}
+			);
+
+			event_hub.connect();
+			console.log('Registered transaction listener with the peer event service for transaction ID:'+ transaction_id_string);
+		});
+
+		promises.push(txPromise);
+
+		console.log('Sending endorsed transaction to the orderer');
+		const results = await Promise.all(promises);
+
+		if (results[0].status === 'SUCCESS') {
+			console.log('Successfully sent transaction to the orderer');
+		} else {
+			const message = util.format('Failed to order the transaction. Error code: %s', results[0].status);
+			console.error(message);
+			throw new Error(message);
+		}
+
+		if (results[1] instanceof Error) {
+			console.error(message);
+			throw new Error(message);
+		} else if (results[1].event_status === 'VALID') {
+			console.log('Successfully committed the change to the ledger by the peer');
+			return({ success: false, message: 'Record inserted into ledger successfully ! ', txnId: transaction_id_string });
+		} else {
+			const message = util.format('Transaction failed to be committed to the ledger due to : %s', results[1].event_status)
+			console.error(message);
+			throw new Error(message);
+		}
+	} catch(error) {
+		console.log('Unable to invoke ::'+ error.toString());
+		return({ success: false, message: 'Unable To Invoke ! ' });
+	}
+}
+
+//Query Hyperledger Fabric Chaincode
+var query = async (channelName, chaincodeId, functionName, arguments, userDoingTxn) => {
+    try {
+
+        const walletPath = path.join(process.cwd(), 'newwallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        const userExists = await wallet.exists(userDoingTxn);
+        if (!userExists) {
+            console.log('An identity for the user' +userDoingTxn+ 'does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userDoingTxn, discovery: { enabled: false } });
+
+        const network = await gateway.getNetwork(channelName);
+
+        const contract = network.getContract(chaincodeId);
+
+        const result = await contract.evaluateTransaction('queryAllCars');
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+		return({ success: true, message: 'Queried successfully ! ', response: ${result.toString()} });
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+		return({ success: false, message: 'Failed to query ! ' });
+    }
 }
 
 app.listen(port);
